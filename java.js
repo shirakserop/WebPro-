d025b45448736a4c9a1f41ee40271d795168a040
Shirak <shirag.serop@gmail.com>
2017-12-18 13:06:47 +0100

    fixing bug with arrays 'circles' 'add_circles'

--- java.js
+++ java.js
@@ -8,18 +8,18 @@ var canvas = document.getElementById('myCanvas'),
          c.fillStyle = "black";
          c.fillRect(0,0,W,H);
 
-         var circles = [],
-              life = 0;
-              maxLife = 100;
+
+              // var life = 0,
+              // maxLife = 100;
 
 function particale (x,y,radius,color){
   this.x = x;
   this.y = y;
-  this.vy = 2;
-  this.vx = 2;
+  this.vy = 0.05;
+  this.vx = 0.05;
   this.radius = radius;
   this.color = color;
-  life++;
+//  life++;
 }
 
 particale.prototype.draw = function(){
@@ -33,9 +33,9 @@ particale.prototype.draw = function(){
 
 particale.prototype.update = function (){
 
-  if(life > maxLife){
-      life = 1;
-  }
+  // if(life > maxLife){
+  //     life = 1;
+  // }
 
   this.x += this.vx;
   this.y += this.vy;
@@ -45,7 +45,31 @@ particale.prototype.update = function (){
 
 
 
+function add_circle(x,y,radius){
+  this.x = x;
+  this.y = y;
+  this.vx = 0.05;
+  this.vy = 0.05; 
+  this.radius= radius;
 
+
+  this.draw = function(){
+    c.beginPath();
+    c.arc(this.x,this.y,this.radius,0,Math.PI * 2);
+    c.fillStyle = "red";
+    c.fill();
+  }
+
+  this.update = function(){
+
+    this.x += this.vx;
+    this.y += this.vy;
+
+    this.draw();
+  }
+
+
+}
 
 
 
@@ -54,22 +78,38 @@ particale.prototype.update = function (){
 
      setInterval(function(){
 
-     c.clearRect(0,0,W,H);
 
-
-     for (var j = 0; j < 200; j++) {
-
-       var x = Math.random() *  W;
-       var y = Math.random() * H;
-       var radius = 20;
-       var color = "rgba(150,105,180,0.5)";
+          c.clearRect(0,0,W,H);
+          var add_circles = [];
+          var circles = [];
+      for (var j = 0; j < 200; j++) {
+           var x = Math.random() *  W;
+           var y = Math.random() * H;
+           var radius = 20;
+           var color = "rgba(150,105,180,0.5)";
 
            var circle = new particale(x,y,radius,color);
            circles.push(circle);
+       }
+
+         for(var i=0; i < 100; i++){
+
+           var x,y,radius,add_circleO;
+           x = Math.random() * W;
+           y = Math.random() * H;
+           radius = Math.random() * 8 ;
+           add_circleO = new add_circle(x,y,radius);
+
+                add_circles.push(add_circleO);
          }
+
+    for(var k = 0; k < add_circles.length; k++){
+        add_circles[k].update();
+    }
+
 
      for (var i = 0; i < circles.length; i++) {
        circles[i].update();
      }
 
-   },1000);
+   },60);
